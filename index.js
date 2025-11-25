2f7833bfc300a5f2cbd3181c126ed1d8b8419224
Brendon W. <bwherry@midland.k12.ia.us>
2025-11-25 10:47:44 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 class TaxCalculator{
     
     constructor(){
-        this.rate = 15;
+        #this.rate = 15;
         this.state = 'TX';
         this.exempt = false;
     }
     
     calculate(){
         if(this.exempt){
-            return calculateExempt(1.37);
+            return #calculateExempt(1.37);
         }else{
-            return calculateNonExempt(5.72);
+            return #calculateNonExempt(5.72);
         }
     }
 }
+
